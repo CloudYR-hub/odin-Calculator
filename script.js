ae31342519decdbcb36d8e324ece12e2a956c877
Rokas Zujus <rokaszuju@gmail.com>
2026-09-17 18:00:48 +0300

    Made the initial calculator design

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+let firstNumber;
+let secondNumber;
+let operation;
+
+function operate(firstNumber, secondNumber, operator) {
+  if (operator === "+") {
+    add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    multiply(firstNumber, secondNumber);
+  } else {
+    divide(firstNumber, secondNumber);
+  }
+}
