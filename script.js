5a6c893c8a02266db360eed3c31e01c675a3290a
CloudYR-hub <rokaszuju@gmail.com>
2026-09-21 19:01:03 +0300

    Added starter calculation logic

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 function subtract(a, b) {
-  return a - b;
+  return Number(a) - Number(b);
 }
 function multiply(a, b) {
-  return a * b;
+  return Number(a) * Number(b);
 }
 function divide(a, b) {
-  return a / b;
+  return Number(a) / Number(b);
 }
-let firstNumber;
-let secondNumber;
-let operation;
+let firstNumber = "";
+let secondNumber = "";
+let operation = "";
 let buttons = document.querySelectorAll('.numberButton');
+let calculateButton = document.getElementById('calculateButton');
+let operations = document.querySelectorAll('.operationButton');
 let input = document.getElementById('input');
 
 console.log(input);
 Array.from(buttons).forEach((button) =>{
   button.addEventListener('click', (e) =>{
-    firstNumber = e.target.innerHTML;
-    console.log(firstNumber);
-    input.value += firstNumber;
+    if(!operation){
+      firstNumber += e.target.innerHTML;
+      input.value += e.target.innerHTML;
+    }
+    else if(operation){
+      secondNumber += e.target.innerHTML;
+      input.value += e.target.innerHTML;
+    }
+    console.log(`First number: ${firstNumber}`);
+    console.log(`Second number: ${secondNumber}`);
+  })
+})
+Array.from(operations).forEach((button) =>{
+  button.addEventListener('click', (e) =>{
+    operation += e.target.innerHTML;
+    input.value += e.target.innerHTML;
+    console.log(typeof(operation));
   })
 })
 function operate(firstNumber, secondNumber, operator) {
   if (operator === "+") {
-    add(firstNumber, secondNumber);
+    return add(firstNumber, secondNumber);
   } else if (operator === "-") {
-    subtract(firstNumber, secondNumber);
+    return subtract(firstNumber, secondNumber);
   } else if (operator === "*") {
-    multiply(firstNumber, secondNumber);
+    return multiply(firstNumber, secondNumber);
   } else {
-    divide(firstNumber, secondNumber);
+    return divide(firstNumber, secondNumber);
   }
 }
+calculateButton.addEventListener('click', (e) =>{
+  input.value = "";
+  let result = operate(firstNumber,secondNumber,operation);
+  input.value = result;
+})
