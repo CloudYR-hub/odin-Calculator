dcb98a32881052c25f00290fc863a301413779fc
Rokas Zujus <rokaszuju@gmail.com>
2026-09-22 18:38:22 +0300

    update 09/22/26

--- script.js
+++ script.js
@@ -13,46 +13,70 @@ function divide(a, b) {
 let firstNumber = "";
 let secondNumber = "";
 let operation = "";
-let buttons = document.querySelectorAll('.numberButton');
-let calculateButton = document.getElementById('calculateButton');
-let operations = document.querySelectorAll('.operationButton');
-let input = document.getElementById('input');
+let clicked = false;
+let buttons = document.querySelectorAll(".numberButton");
+let calculateButton = document.getElementById("calculateButton");
+let clearButton = document.getElementById("clearButton");
+let operations = document.querySelectorAll(".operationButton");
+let input = document.getElementById("input");
 
 console.log(input);
-Array.from(buttons).forEach((button) =>{
-  button.addEventListener('click', (e) =>{
-    if(!operation){
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (!operation) {
       firstNumber += e.target.innerHTML;
       input.value += e.target.innerHTML;
-    }
-    else if(operation){
+    } else if (operation) {
       secondNumber += e.target.innerHTML;
       input.value += e.target.innerHTML;
     }
     console.log(`First number: ${firstNumber}`);
     console.log(`Second number: ${secondNumber}`);
-  })
-})
-Array.from(operations).forEach((button) =>{
-  button.addEventListener('click', (e) =>{
-    operation += e.target.innerHTML;
-    input.value += e.target.innerHTML;
-    console.log(typeof(operation));
-  })
-})
+  });
+});
+Array.from(operations).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (!operation) {
+      operation = e.target.innerHTML;
+      input.value += e.target.innerHTML;
+    } else if (operation) {
+      let result = operate(firstNumber, secondNumber, operation);
+      secondNumber = "";
+      operation = e.target.innerHTML;
+      input.value = result + operation;
+      firstNumber = result;
+    }
+  });
+});
 function operate(firstNumber, secondNumber, operator) {
   if (operator === "+") {
-    return add(firstNumber, secondNumber);
+    return Number.parseFloat(add(firstNumber, secondNumber)).toFixed(0);
   } else if (operator === "-") {
-    return subtract(firstNumber, secondNumber);
+    return Number.parseFloat(subtract(firstNumber, secondNumber)).toFixed(0);
   } else if (operator === "*") {
-    return multiply(firstNumber, secondNumber);
+    return Number.parseFloat(multiply(firstNumber, secondNumber)).toFixed(0);
   } else {
-    return divide(firstNumber, secondNumber);
+    if (secondNumber === "0") {
+      return (input.value = "Cannot divide from 0");
+    } else {
+      return Number.parseFloat(divide(firstNumber, secondNumber)).toFixed(0);
+    }
   }
 }
-calculateButton.addEventListener('click', (e) =>{
+calculateButton.addEventListener("click", (e) => {
+  if (!operation || !firstNumber || !secondNumber) {
+    return;
+  }
   input.value = "";
-  let result = operate(firstNumber,secondNumber,operation);
+  let result = operate(firstNumber, secondNumber, operation);
   input.value = result;
-})
+  secondNumber = "";
+  operation = "";
+  firstNumber = result;
+});
+clearButton.addEventListener("click", () => {
+  input.value = "";
+  operation = "";
+  firstNumber = "";
+  secondNumber = "";
+});
